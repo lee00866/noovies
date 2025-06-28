e7cdeea1763ee53b0330e8d3bd66336092c788c4
Eunji Lee <lee00866@algonquinlive.com>
2025-06-29 04:09:18 +0900

    Add Infinite Scroll for the Upcoming movies

--- screens/Movies.jsx
+++ screens/Movies.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { Dimensions, FlatList, useColorScheme } from "react-native";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  useInfiniteQuery,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import Swiper from "react-native-swiper";
 import styled from "styled-components/native";
 import { BLACK_COLOR } from "../colors";
@@ -44,35 +48,51 @@ const Movies = () => {
   const isDark = useColorScheme() === "dark";
   const backgroundColor = isDark ? BLACK_COLOR : "white";
 
+  const queryClient = useQueryClient();
+  const [refreshing, setRefreshing] = useState(false);
+
   const { isLoading: nowPlayingLoading, data: nowPlayingData } = useQuery({
     queryKey: ["movies", "nowPlaying"],
     queryFn: moviesApi.nowPlaying,
   });
-  const { isLoading: upcomingLoading, data: upcomingData } = useQuery({
+  const {
+    isLoading: upcomingLoading,
+    data: upcomingData,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
     queryKey: ["movies", "upcoming"],
     queryFn: moviesApi.upcoming,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      const nextPage = lastPage.page + 1;
+      return nextPage > lastPage.total_pages ? null : nextPage;
+    },
   });
   const { isLoading: trendingLoading, data: trendingData } = useQuery({
     queryKey: ["movies", "trending"],
     queryFn: moviesApi.trending,
   });
 
-  const queryClient = useQueryClient();
-  const [refreshing, setRefreshing] = useState(false);
   const onRefresh = async () => {
     setRefreshing(true);
     await queryClient.refetchQueries(["movies"]);
     setRefreshing(false);
   };
 
-  const movieKeyExtractor = (item) => item.id;
-
   const loading = nowPlayingLoading || upcomingLoading || trendingLoading;
+
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
 
   return loading ? (
     <Loader />
   ) : upcomingData ? (
     <FlatList
+      onEndReached={loadMore}
       style={{ backgroundColor }}
       onRefresh={onRefresh}
       refreshing={refreshing}
@@ -109,21 +129,29 @@ const Movies = () => {
           <ComingSoonTitle>Coming Soon</ComingSoonTitle>
         </>
       }
-      data={upcomingData.results}
-      keyExtractor={movieKeyExtractor}
+      data={upcomingData.pages.map((page) => page.results).flat()}
+      keyExtractor={(item) => item.id + ""}
       ItemSeparatorComponent={HSeparator}
-      renderItem={({ item }) => (
-        <HMedia
-          posterPath={item.poster_path}
-          originalTitle={item.original_title}
-          overview={item.overview}
-          releaseDate={item.release_date}
-          voteAverage={item.vote_average}
-          fullData={item}
-        />
-      )}
+      renderItem={({ item }) => {
+        if (!item.poster_path) return null;
+        return (
+          <HMedia
+            posterPath={item.poster_path || ""}
+            originalTitle={item.original_title}
+            overview={item.overview}
+            releaseDate={item.release_date}
+            voteAverage={item.vote_average}
+            fullData={item}
+          />
+        );
+      }}
     />
   ) : null;
 };
 
 export default Movies;
+
+// {"pageParams": [1],
+//   "pages": [
+//     {"dates": [Object], "page": 1, "results": [Array], "total_pages": 39, "total_results": 772}
+//   ]}

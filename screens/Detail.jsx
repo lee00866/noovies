a5a6384307a570b7e7368db9761ae53b0453c177
Eunji Lee <lee00866@algonquinlive.com>
2025-06-28 11:27:11 +0900

    Change background for the Detail Screen

--- screens/Detail.jsx
+++ screens/Detail.jsx
@@ -1,24 +1,79 @@
 import { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 import Poster from "../components/Poster";
+import { makeImgPath } from "../utils";
+
+const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0px 20px;
+  position: relative;
+`;
+
+const Background = styled.Image`
+  opacity: 0.6;
+`;
+
+const FadeOverlay = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+`;
+
+const Column = styled.View`
+  flex-direction: row;
+  width: 80%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 36px;
+  align-self: flex-end;
+  width: 80%;
+  margin-left: 15px;
+  font-weight: 500;
+`;
+
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 20px;
+  padding: 0px 20px;
+`;
+
 const Detail = ({ navigation: { setOptions }, route: { params } }) => {
   useEffect(() => {
     setOptions({
-      title:
-        "original_title" in params
-          ? params.original_title
-          : params.original_name,
+      title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
   return (
     <Container>
-      <Poster path={params.poster_path || ""} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || null) }}
+        />
+        <Column>
+          <FadeOverlay />
+          <Poster path={params.poster_path || ""} />
+          <Title>
+            {"original_title" in params
+              ? params.original_title
+              : params.original_name}
+          </Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
     </Container>
   );
 };
